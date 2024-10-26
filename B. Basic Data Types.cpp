c51efa7a24df6d90150047ffd0bef10d81df88c6
Priyanshu Jha <95862487+pandit27@users.noreply.github.com>
2024-10-26 17:38:44 +0530

    Create B. Basic Data Types.cpp

---
+++ B. Basic Data Types.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+using ll = long long;
+
+int main() {
+    int a;
+    ll b;
+    char c;
+    float d;
+    double e;
+    cin >> a >> b >> c >> d >> e;
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << d << endl;
+    cout << e << endl;
+}
