79a365d3081bb0a9d032ca58b766ad72414b37b9
Priyanshu Jha <95862487+pandit27@users.noreply.github.com>
2024-10-27 17:40:06 +0530

    Create F. Reversing.cpp

---
+++ F. Reversing.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+using ll = long long;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    ll n;
+    cin >> n;
+    vector<ll> v(n);
+    for (int i = 0; i < n; i++) cin >> v[i];
+    for (int i = n - 1; i >= 0; i--) cout << v[i] << " ";
+}
