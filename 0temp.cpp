7f1e21003f1686169e8ba02ffc12d51541c44a2b
Priyanshu Jha <95862487+pandit27@users.noreply.github.com>
2024-11-25 18:35:59 +0530

    Update 0temp.cpp

--- 0temp.cpp
+++ 0temp.cpp
@@ -1,7 +1,5 @@
-#include <iostream>
-using namespace std;
-using ll = long long;
-
-int main() {
-   
+for(int i = n; i > 0; i /= 2) {
+    for(int j = 0; j < i; j++) {
+        // Some constant time operation
+    }
 }
